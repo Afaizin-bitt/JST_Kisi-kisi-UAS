a5b7aca071a2779d451a5a7d4397c8af16137208
Afaizin-bitt <84916753+Afaizin-bitt@users.noreply.github.com>
2021-06-26 01:39:08 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -1,38 +1,30 @@
 const tf = require('@tensorflow/tfjs-node');
 
-function input(data){ // i & r
-    a = (data[1]) 
-    b = (data[2])
-    c = (data[3])
-    return [a, b, c]
+function normalized(data){ // i & r
+    x = (data[1]) 
+    y = (data[2])
+    z = (data[3])
+    return [x, y, z]
 }
-
-function output(data){
-    d = (data[4])
-    e = (data[5])
-    f = (data[6])
-    return [d, e, f]
-}
-
 
 async function predict(data){
     let in_dim = 3;
     
-    data = input(data);
+    data = normalized(data);
     shape = [1, in_dim];
 
     tf_data = tf.tensor2d(data, shape);
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/Afaizin-bitt/JST_Kisi-kisi-UAS/main/public/Ex-Model/model.jsonhttps://raw.githubusercontent.com/Afaizin-bitt/JST_Kisi-kisi-UAS/main/public/Ex-Model/model.json';
+        const path = 'raw.githubusercontent.com/Afaizin-bitt/JST_Kisi-kisi-UAS/main/public/Ex-Model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
                 tf_data
         );
         result = predict.dataSync();
-        return output( result );
+        return normalized( result );
         
     }catch(e){
       console.log(e);
