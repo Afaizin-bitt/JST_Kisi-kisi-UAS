d2997918c60d24c10cb507f8ab7b71c1a1f1db1d
Afaizin-bitt <84916753+Afaizin-bitt@users.noreply.github.com>
2021-06-26 02:57:09 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -42,7 +42,7 @@ bot.on('message',(msg) => {
         ).then((jres1)=>{
             console.log(jres1);
                 
-            cls_model.predict([parseFloat(s[1]), parseFloat(s[2]), parseFloat(s[3]), parseFloat(jres1[1]), parseFloat(jres1[2], parseFloat(jres1[3])])=>{
+            cls_model.predict([parseFloat(s[1]), parseFloat(s[2]), parseFloat(s[3]), parseFloat(jres1[1]), parseFloat(jres1[2]), parseFloat(jres1[2])])=>{
                 bot.sendMessage(
                     msg.chat.id,
                     `nilai v yang diprediksi adalah ${jres1[1]} volt`
@@ -53,7 +53,7 @@ bot.on('message',(msg) => {
                     );
                 bot.sendMessage(
                     msg.chat.id,
-                    `nilai p yang diprediksi adalah ${jres1[3]} watt`
+                    `nilai p yang diprediksi adalah ${jres1[2]} watt`
                     );
                     
             })
