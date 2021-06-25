0188e62e3bf65306b170c304e24492cc86826404
Afaizin-bitt <84916753+Afaizin-bitt@users.noreply.github.com>
2021-06-26 03:36:03 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -29,5 +29,48 @@ bot.onText(/\/predict/, (msg) => {
     );
     state = 1   
 });
+bot.on('message',(msg) => {
+    if(state == 1){
+        s = msg.text.split("|");
+        model.predict(
+            [
+                parseFloat (s[1]),
+                parseFloat (s[2]),
+                parseFloat (s[3])
+            ]
+        ).then((jres1)=>{
+            console.log(jres1);
+                
+            cls_model.classify([parseFloat(s[1]), parseFloat(s[2]), parseFloat(s[3]), parseFloat(jres1[1]), parseFloat(jres1[2])]).then((jres2)=>{
+                bot.sendMessage(
+                    msg.chat.id,
+                    `nilai v yang diprediksi adalah ${jres1[1]} volt`
+                    );
+                bot.sendMessage(
+                    msg.chat.id,
+                    `nilai p yang diprediksi adalah ${jres1[2]} watt`
+                    );
+                 bot.sendMessage(
+                    msg.chat.id,
+                    `Klasifikasi Tegangan ${jres2}`
+                    );
+                    
+            })
+            })
+    }
+})
+// routers
+r.get('/predict/:x/:y/:z', function(req, res, next) {    
+    model.predict(
+        [
+            parseFloat(req.params.x), // string to float
+            parseFloat(req.params.y),
+            parseFloat(req.params.z)
+        ]
+    ).then((jres)=>{
+        res.json(jres);
+    })
+});
+
 
 module.exports = r;
