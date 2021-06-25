151d4d3ae1c360c449ba4665df7aaf0856a5da86
Afaizin-bitt <84916753+Afaizin-bitt@users.noreply.github.com>
2021-06-26 01:33:22 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '1882953763:AAGtAdcnQXNe2bogE17VQjdwmUUcgieYHBU'
 const bot = new TelegramBot(token, {polling: true});
 
 
@@ -16,24 +16,58 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click /predict to main predict`
     );   
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
-    bot.sendMessage(
+state = 0;
+bot.onText(/\/predict/, (msg) => { 
+        bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
-    );   
+        `Masukkan nilai x|y|z seperti 2|2|2`
+    );
+    state = 1   
 });
+
+bot.on('message',(msg) => {
+    if(state == 1){
+        s = msg.text.split("|");
+        x = s[1]
+        y = s[2]
+        z = s[3]
+        model.predict(
+            [
+                parseFloat (s[1]),
+                parseFloat (s[2]),
+                parseFloat (s[3])
+            ]
+        ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai M1 yang diprediksi adalah ${jres[1]} derajat`  
+            );
+           bot.sendMessage(
+               msg.chat.id,
+               `nilai M2 yang diprediksi adalah ${jres[2]} derajat`
+           );
+            bot.sendMessage(
+               msg.chat.id,
+               `nilai M3 yang diprediksi adalah ${jres[3]} derajat`
+           );
+        })
+    }else{
+        state = 0
+    }
+})
+
 
 // routers
-r.get('/prediction/:i/:r', function(req, res, next) {    
+r.get('/prediction/:x/:y/z', function(req, res, next) {    
     model.predict(
         [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r)
+            parseFloat(req.params.x), // string to float
+            parseFloat(req.params.y),
+            parseFloat(req.params.z)
         ]
     ).then((jres)=>{
         res.json(jres);
