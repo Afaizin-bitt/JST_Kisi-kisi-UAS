f883eb53301992177360e7c024430eda2420d680
Afaizin-bitt <84916753+Afaizin-bitt@users.noreply.github.com>
2021-06-26 09:12:18 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -41,7 +41,7 @@ bot.on('message',(msg) => {
         ).then((jres1)=>{
             console.log(jres1);
                 
-            cls_model.predict([parseFloat(s[1]), parseFloat(s[2]), parseFloat(s[3]), parseFloat(jres1[1]), parseFloat(jres1[2])])=>{
+            cls_model.predict([parseFloat(s[1]), parseFloat(s[2]), parseFloat(s[3]), parseFloat(jres1[1]), parseFloat(jres1[2])]).then((jres2)=>{
                 bot.sendMessage(
                     msg.chat.id,
                     `nilai M1 yang diprediksi adalah ${jres1[1]} derajat`
